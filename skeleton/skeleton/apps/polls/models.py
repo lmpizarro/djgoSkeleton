96e54a630f0df536257feae0db8a82ffc283cf63
lmpizarro <lmpbici@gmail.com>
2013-04-10 20:32:04 -0300

    End Tutorial part2

--- skeleton/skeleton/apps/polls/models.py
+++ skeleton/skeleton/apps/polls/models.py
@@ -1,3 +1,5 @@
+import datetime
+from django.utils import timezone
 from django.db import models
 
 # Create your models here.
@@ -10,6 +12,9 @@ class Poll(models.Model):
         return self.question
     def was_published_recently(self):
         return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+    was_published_recently.admin_order_field = 'pub_date'
+    was_published_recently.boolean = True
+    was_published_recently.short_description = 'Published recently?'
 
 
 class Choice(models.Model):
