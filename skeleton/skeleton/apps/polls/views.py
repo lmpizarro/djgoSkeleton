647744f1a036c41454e10b17d5b57780920b37c4
lmpizarro <lmpbici@gmail.com>
2013-04-23 19:48:08 -0300

    tutorial part3 end

--- skeleton/skeleton/apps/polls/views.py
+++ skeleton/skeleton/apps/polls/views.py
@@ -1,1 +1,38 @@
 # Create your views here.
+
+from django.http import HttpResponse
+from django.template import Context, loader
+from django.http import Http404
+
+
+from django.shortcuts import render, get_object_or_404
+
+from skeleton.apps.polls.models import Poll
+
+
+def index(request):
+    latest_poll_list = Poll.objects.all().order_by('-pub_date')[:5]
+    context = {'latest_poll_list': latest_poll_list}
+    return render(request, 'polls/index.html', context)
+
+#
+#  detail sin shortcut
+#
+def detail01(request, poll_id):
+    try:
+        poll = Poll.objects.get(pk=poll_id)
+    except Poll.DoesNotExist:
+        raise Http404
+    return render(request, 'polls/detail.html', {'poll': poll})
+
+
+def detail(request, poll_id):
+    poll = get_object_or_404(Poll, pk=poll_id)
+    return render(request, 'polls/detail.html', {'poll': poll})
+
+
+def results(request, poll_id):
+    return HttpResponse("You're looking at the results of poll %s." % poll_id)
+
+def vote(request, poll_id):
+    return HttpResponse("You're voting on poll %s." % poll_id)
