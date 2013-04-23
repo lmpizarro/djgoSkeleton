4d5ec7a67b24222c7da9ad13e91abb707f9be84a
lmpizarro <lmpbici@gmail.com>
2013-04-23 20:15:53 -0300

    tutorial part 4 before generic views

--- skeleton/skeleton/apps/polls/views.py
+++ skeleton/skeleton/apps/polls/views.py
@@ -1,13 +1,15 @@
 # Create your views here.
 
-from django.http import HttpResponse
+from django.http import HttpResponseRedirect, HttpResponse
 from django.template import Context, loader
 from django.http import Http404
 
 
 from django.shortcuts import render, get_object_or_404
 
-from skeleton.apps.polls.models import Poll
+from skeleton.apps.polls.models import Poll, Choice
+
+from django.core.urlresolvers import reverse
 
 
 def index(request):
@@ -32,7 +34,23 @@ def detail(request, poll_id):
 
 
 def results(request, poll_id):
-    return HttpResponse("You're looking at the results of poll %s." % poll_id)
+    poll = get_object_or_404(Poll, pk=poll_id)
+    return render(request, 'polls/results.html', {'poll': poll})
 
 def vote(request, poll_id):
-    return HttpResponse("You're voting on poll %s." % poll_id)
+    p = get_object_or_404(Poll, pk=poll_id)
+    try:
+        selected_choice = p.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        # Redisplay the poll voting form.
+        return render(request, 'polls/detail.html', {
+            'poll': p,
+            'error_message': "You didn't select a choice.",
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        # Always return an HttpResponseRedirect after successfully dealing
+        # with POST data. This prevents data from being posted twice if a
+        # user hits the Back button.
+        return HttpResponseRedirect(reverse('polls:results', args=(p.id,)))
