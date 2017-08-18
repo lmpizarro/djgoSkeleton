b0895d3b2f5b9ec6fd59fdddfae21720ea4b69eb
Luis María Pizarro <lmpizarro@gmail.com>
2017-08-18 13:07:41 -0300

    token auth added to catalog

--- skel02/DjngPrjEsq/urls.py
+++ skel02/DjngPrjEsq/urls.py
@@ -13,20 +13,28 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from rest30lines.views import FileViewSet
-from rest_framework.routers import DefaultRouter
-
-
+#from rest30lines.views import FileViewSet
 from django.conf.urls import include, url
+from rest_framework.routers import DefaultRouter
+from rest_framework.authtoken.views import obtain_auth_token
+from quickstart import views
+
 from django.contrib import admin
 
+import catalog
 
 router = DefaultRouter()
-router.register(r'rest30lines', FileViewSet)
+#router.register(r'rest30lines', FileViewSet)
+router.register(r'user', views.UserViewSet)
+router.register(r'groups', views.GroupViewSet)
 
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'', include('myApp01.urls')),
-    url(r'^rest30lines/', include(router.urls)),
+    #url(r'^accounts/profile/', admin.site.urls),
+    #url(r'', include('myApp01.urls')),
+    #url(r'^rest30lines/', include(router.urls)),
+    url(r'^api-token-auth/', obtain_auth_token),
+    url(r'^', include('catalog.urls')),
+    #url(r'^auth/', include('rest_framework.urls', namespace='rest_framework')),
 ]
