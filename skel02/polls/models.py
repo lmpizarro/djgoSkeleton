ccaf5c639545a9021940eaac0acaf8069419f181
Luis María Pizarro <lmpizarro@gmail.com>
2017-08-23 14:11:35 -0300

    admin custom t 7

--- skel02/polls/models.py
+++ skel02/polls/models.py
@@ -1,31 +1,39 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
 from django.db import models
+from django.utils.encoding import python_2_unicode_compatible
 
 # Create your models here.
-
+from django.utils import timezone
 from django.db import models
-
 import datetime
 
+@python_2_unicode_compatible  # only if you need to support Python 2
 class Question(models.Model):
-      question_text = models.CharField(max_length=200)
-      pub_date = models.DateTimeField('date published')
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
 
-      def __str__(self):
-          return self.question_text
+    def __str__(self):
+        return self.question_text
 
-      def was_published_recently(self):
-          return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+    def was_published_recently(self):
+        now = timezone.now()
+        delta = now - datetime.timedelta(days=1) 
+        print "Delta: %s %s; %s"%(delta, self.pub_date, datetime.timedelta(days=1))
+        print " <= ", delta <= self.pub_date
+        return delta <= self.pub_date <= now
+        
+    was_published_recently.admin_order_field = 'pub_date'
+    was_published_recently.boolean = True
+    was_published_recently.short_description = 'Published recently?'
 
 
 
+@python_2_unicode_compatible  # only if you need to support Python 2
 class Choice(models.Model):
       question = models.ForeignKey(Question, on_delete=models.CASCADE)
       choice_text = models.CharField(max_length=200)
       votes = models.IntegerField(default=0)
-
 
       def __str__(self):
           return self.choice_text
